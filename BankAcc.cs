9761355b3a1b61eef7bce53bbb2697209d999197
Harold Bengter <Haroldbe55@yahoo.com>
2022-07-14 21:16:34 -0400

    Finsihed all tasks

---
+++ BankAcc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace BankEncapsulation;
+
+public class BankAcc
+{
+    private double balance = 0;
+
+    public void Deposit(double userAmount)
+    {
+        
+        balance += userAmount;
+    }
+
+    public double Getbalance()
+    {
+        return balance;
+    }
+
+    public void Withdraw(double userAmount)
+    {
+        balance -= userAmount;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using BankEncapsulation;
+
+var bankAccount = new BankAcc();
+
+Console.WriteLine($"Welcome to Bengter Bank, your acc balance is ${bankAccount.Getbalance()}");
+
+Console.WriteLine("Please select from the list: Withdraw or Deposit");
+var answer = Console.ReadLine();
+if (answer == "Withdraw")
+{
+    Console.WriteLine("How much would you like to Withdrawl?");
+    double withdraw = double.Parse(Console.ReadLine());
+    bankAccount.Withdraw(withdraw);
+    Console.WriteLine($"Your balance is now: ${bankAccount.Getbalance()}");
+    Console.WriteLine("Thank you for choosing Bengter Bank. Have a great day!");
+}
+
+else if (answer == "Deposit")
+{
+    Console.WriteLine("How much would you like to deposit?");
+    double deposit = double.Parse(Console.ReadLine());
+    bankAccount.Deposit(deposit);
+    Console.WriteLine($"Your balance is now:  ${bankAccount.Getbalance()}");
+    Console.WriteLine("Thank you for choosing Bengter Bank. Have a great day!");
+}
+
+else
+{
+    Console.WriteLine("Goodbye");
+}
+
+
+
+
+
+
+
